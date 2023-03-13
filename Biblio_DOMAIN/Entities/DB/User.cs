dc6d2cacd8d7a349669edbf9905898c902eac89a
KraSS <groverkras@gmail.com>
2023-03-13 22:32:36 +0200

    Create User and Location Entity

---
+++ Biblio_DOMAIN/Entities/DB/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblio_DOMAIN.Entities.DB
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Description { get; set; }
+
+        public int LocationId { get; set; }
+        public Location Location { get; set; }
+       
+        public bool Followed { get; set; }
+    }
+}
