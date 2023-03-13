928e50c7c481e94816c1472377e580a144f59dbf
KraSS <groverkras@gmail.com>
2023-03-13 22:44:03 +0200

    Create and Configure AppDbContext

--- BIBLIO_DAL/Biblio_DAL/AppDbContext.cs
+++ BIBLIO_DAL/Biblio_DAL/AppDbContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Biblio_DOMAIN.Entities.DB;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,44 @@ using System.Threading.Tasks;
 
 namespace Biblio_DAL
 {
-    internal class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public DbSet<Location> Locations { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options) { }
+        
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Location>(entity =>
+            {
+                entity.HasKey(l => l.Id).HasName("Primary");
+
+                entity.ToTable("Locations");
+
+                entity.Property(l => l.Id).HasColumnName("Id");
+                entity.Property(l => l.Country).HasColumnName("Country");
+                entity.Property(l => l.City).HasColumnName("City");
+                
+            });
+
+            builder.Entity<User>(entity =>
+            {
+                entity.HasKey(u => u.Id).HasName("Primary");
+
+                entity.ToTable("Users");
+
+                entity.Property(u => u.Id).HasColumnName("Id");
+                entity.Property(u => u.UserName).HasColumnName("UserName");
+                entity.Property(u => u.Description).HasColumnName("Description");
+                entity.Property(u => u.LocationId).HasColumnName("Location");
+                entity.Property(u => u.Followed).HasColumnName("Followed");
+
+                entity.HasOne(u => u.Location)
+                .WithMany(l => l.Users)
+                .HasForeignKey(u => u.LocationId);
+            });
+        }
 
     }
 }
