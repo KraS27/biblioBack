e22e80f2add52704c76e9f45fb8e0cf000076063
KraSS <groverkras@gmail.com>
2023-03-13 22:21:15 +0200

    Add project files.

---
+++ biblioBack/Controllers/UserController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace biblioBack.Controllers
+{
+
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {     
+        [HttpGet("GetUsers")]
+        public IEnumerable<string> GetUsers()
+        {
+            return new List<string> { "Sasha", "Pasha", "Dasha", "Kasha" };
+        }
+    }
+}
